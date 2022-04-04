156dced1dfeb6bc41cc04b361b6d3b35de8f54f0
Jesse Meza <jmeza@spoton.com>
2022-04-04 15:50:05 -0500

    add sample for Yaml tokens instead

---
+++ build/javascript/colors.js
@@ -0,0 +1,9 @@
+/**
+ * Do not edit directly
+ * Generated on Mon, 04 Apr 2022 20:49:28 GMT
+ */
+
+module.exports = {
+  "ColorBackgroundDangerNeutral": "#EEF5FF",
+  "ColorBackgroundSuccessDefaultHover": "#001A57"
+};
