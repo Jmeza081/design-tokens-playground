fde9ece967bc76047225be7b8090d90966d89bc6
Jesse Meza <jmeza@spoton.com>
2022-04-06 11:41:30 -0500

    Setup Gradient transform examples

--- build.js
+++ build.js
@@ -7,16 +7,19 @@ console.log('\n==============================================');
 // REGISTER THE CUSTOM TRANFORMS
 
 StyleDictionary.registerTransform({
-  name: 'gradient/unify',
+  name: 'color/gradient',
   type: 'value',
   matcher: function(token) {
-      console.log(token.attributes);
+    if (token.meta){
       // this is an example of a possible filter (based on the "cti" values) to show how a "matcher" works
-      return token.attributes.subitem === 'gradient';
+      return token.meta.type === 'gradient';
+    }
+
+    return false;
   },
   transformer: function(token) {
-      return "Test";
-    //   return `${token.value.from} + ${token.value.to}`;
+    console.log(token);
+    return `linear-gradient(to left top, ${token.original.value.from}, ${token.original.value.to})`
   }
 });
 
@@ -30,7 +33,7 @@ StyleDictionary.registerTransformGroup({
   name: 'scss',
   // this is to show one possibility for adding a few transforms to a pre-defined group
   // (however, we suggest to use the previous approach, which is more explicit and clear)
-  transforms: StyleDictionary.transformGroup['scss'].concat(['gradient/unify'])
+  transforms: StyleDictionary.transformGroup['scss'].concat(['color/gradient'])
 });
 
 
--- build/javascript/colors.js
+++ build/javascript/colors.js
@@ -1,9 +1,11 @@
 /**
  * Do not edit directly
- * Generated on Mon, 04 Apr 2022 20:49:28 GMT
+ * Generated on Tue, 05 Apr 2022 21:27:19 GMT
  */
 
 module.exports = {
   "ColorBackgroundDangerNeutral": "#EEF5FF",
-  "ColorBackgroundSuccessDefaultHover": "#001A57"
+  "ColorBackgroundSuccessDefaultHover": "#001A57",
+  "ColorGradientLoyalty": "#000000",
+  "GlobalColorBlue05": "#EEF5FF"
 };
